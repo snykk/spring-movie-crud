5d94b6566255364e409850562df899f051079a46
Najib Fikri <najibfikri13@gmail.com>
2024-11-15 16:12:30 +0700

    Add starter Spring Boot project

---
+++ src/main/java/com/example/springmoviecrud/SpringMovieCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmoviecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMovieCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMovieCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmoviecrud/SpringMovieCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmoviecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMovieCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
